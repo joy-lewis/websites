7921c847e996b1e8896d84371194a2dfba0c5a24
joy-lewis <joylucas912@gmail.com>
2026-06-07 13:06:26 +0200

    bio

--- app/bio/page.tsx
+++ app/bio/page.tsx
@@ -5,9 +5,7 @@ export default function Bio() {
       <p style={{ marginTop: '2rem' }}>
         Based in the historic city of Maastricht, Netherlands, Wandi de Carvalho Lucas is a visual artist dedicated to capturing the essence of form and color through contemporary painting.
       </p>
-      <p style={{ marginTop: '1.5rem' }}>
-        Her work explores the intersection of traditional techniques and modern perspectives, often drawing inspiration from the rich cultural landscape of the Limburg region.
-      </p>
+      
     </div>
   )
 }
